b7f93b1a79e2559a0e5d83a24ba95511418cc7f1
Dawid Eder <dawideder@gmail.com>
2020-07-06 21:41:20 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,141 @@
+//data types w html uzyliśmy, zeby nie mieszać klas css z javascriptem//
+//wszystko zasadniczo przypisujemy w JS do zmiennych :P // 
+
+
+
+
+//drugi krok, zdefiniowanie zadań kalkulatora w klasie//
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement; 
+        this.currentOperandTextElement = currentOperandTextElement; 
+        this.clear()
+    }
+
+clear(){     //czyści wszystko//
+    this.currentOperand = ''; 
+    this.previousOperand = ''; 
+    this.operation = undefined; 
+
+}
+delete(){    //usunięcie pojedynczego numeru//
+this.currentOperand = this.currentOperand.toString().slice(0, -1)
+}
+appendNumber(number){      //dodaje liczbę do ekranu po kliknięciu//
+if (number === '.' && this.currentOperand.includes('.')) return 
+this.currentOperand = this.currentOperand.toString() + number.toString()
+}
+chooseOperation(operation){   //mnozenie, dzielenie, dodanie, minus, co wybierze uzytkownik//
+if (this.currentOperand === '') return 
+if (this.previousOperand !== '') {
+    this.compute()
+}
+this.operation = operation
+this.previousOperand = this.currentOperand
+this.currentOperand = ''
+}
+compute(){       //wyliczenie ostatecznego wyniku, sumy//
+let computation 
+const prev = parseFloat(this.previousOperand)
+const current = parseFloat(this.currentOperand)
+if (isNaN(prev) || isNaN(current)) return
+switch (this.operation) {
+    case '+':
+        computation = prev + current
+        break
+    case '-':
+        computation = prev - current
+        break
+    case '*':
+        computation = prev * current
+        break
+    case '÷':
+        computation = prev / current
+        break
+    default:
+        return
+}
+this.currentOperand = computation
+this.operation = undefined
+this.previousOperand = ''
+}
+
+getDisplayNumber(number) {
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay 
+    if (isNaN(integerDigits)) {
+        integerDisplay = ''
+    }
+    else {
+        integerDisplay = integerDigits.toLocaleString('en', {
+        maximumFractionDigits: 0 })
+    }
+    if (decimalDigits != null){
+        return `${integerDisplay}.${decimalDigits}`
+    } else {
+        return integerDisplay
+    }
+}
+
+updateDisplay(){
+this.currentOperandTextElement.innerText = 
+this.getDisplayNumber(this.currentOperand)
+if (this.operation != null) {
+    this.previousOperandTextElement.innerText = 
+    `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+}
+else {
+    this.previousOperandTextElement.innerText = '' //wyczyszczenie góry//
+}
+}
+}
+
+
+
+//pobieranie wszystkich potrzebnych danych, pierwszy krok 
+
+const numberButtons = document.querySelectorAll('[data-number'); 
+const operationButtons = document.querySelectorAll('[data-operation'); 
+const equalsButton = document.querySelector('[data-equals'); 
+const deleteButton = document.querySelector('[data-delete'); 
+const allClearButton = document.querySelector('[data-all-clear'); 
+const previousOperandTextElement = document.querySelector('[data-previous-operand'); 
+const currentOperandTextElement = document.querySelector('[data-current-operand'); 
+
+
+//trzeci krok, stworzenie obiektu kalkulator i pętli, która wykonuje zadania// 
+
+
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {button.addEventListener('click', () => {
+calculator.appendNumber(button.innerText)
+calculator.updateDisplay()})
+
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
